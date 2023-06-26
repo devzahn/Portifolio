d39811392bfb1b366567556d13e2368d6f74884a
Eduardo Tomazelli <eduardozahntomazelli@gmail.com>
2023-06-26 16:45:37 -0300

    26/06/23
    ajustado img dos projetos

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-function makeLatinSquare(n) {
-  let num = []; 
-  let generate = [];
-  for (let i = 1; i <= n; i++) {
-    generate.push(i)
-    num.push(generate);
-  }
+/* 
+const projetoSection = document.getElementById('image-container-project');
 
-  for(let i = 1; i < n; i++ ){
-    let rotor = generate[i - 1]; /* adiciona o rotor antes de generate */
-    let novoRotor = rotor.slice(1).concat(rotor[0]);/* A linha anterior é rotacionada e remove o elemento anterior, criando uma nova linha */
 
-    generate.push(novoRotor);
-  }
+const imagens = [
+  'assets/Cronometro.jpg',
+  'assets/loginpage.jpg',
+  'assets/Financas.jpg'
+];
 
-  return generate;
-}
+imagens.forEach((imagemSrc) => {
+
+  const imagemElement = document.createElement('img');
+
+  imagemElement.src = imagemSrc;
+  projetoSection.appendChild(imagemElement);
+});
+ */
