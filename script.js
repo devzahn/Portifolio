19b26a165d7863c12337ddd891272acb9c47eed9
Eduardo Tomazelli <eduardozahntomazelli@gmail.com>
2023-08-25 21:18:17 -0300

    25/08/2023
    Modificada img

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+
+
 function ativaletras(elemento) {
-    const arrTexto = elemento.innerHTML.split('')
-    elemento.innerHTML = '';
-    arrTexto.forEach((letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += letra;
-        }, 50 * i)
-    })
+        const arrTexto = elemento.innerHTML.split('')
+        elemento.innerHTML = '';
+        arrTexto.forEach((letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += letra;
+            }, 50 * i)
+        })
 }
 
 const titulo = document.querySelector('.typing');
+ativaletras(titulo); 
 
-ativaletras(titulo);
+
+const ativaMenu = document.querySelector('.fa-bars');
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+})
